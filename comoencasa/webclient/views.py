47de781966d3d7fff2eca5c85fde4dba652fe282
Daniel Felipe Montoya Ramirez <dmontoya@apptitud.com.co>
2019-01-30 12:40:11 -0500

    Updating envs production

--- comoencasa/webclient/views.py
+++ comoencasa/webclient/views.py
@@ -28,8 +28,3 @@ class Home(TemplateView):
 class RoomDetailView(DetailView):
 
     model = Room
-
-    # def get_context_data(self, **kwargs):
-    #     context = super().get_context_data(**kwargs)
-    #     context['now'] = timezone.now()
-    #     return context
