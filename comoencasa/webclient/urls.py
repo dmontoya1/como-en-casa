eaf7184885ce223f51f01dff09201502e28be002
Daniel Felipe Montoya Ramirez <dmontoya@apptitud.com.co>
2019-02-27 23:14:06 -0500

    Updating New Template

--- comoencasa/webclient/urls.py
+++ comoencasa/webclient/urls.py
@@ -11,12 +11,12 @@ app_name = 'webclient'
 urlpatterns = [
     path("", views.Home.as_view(), name="home"),
     path("about/", views.AboutUs.as_view(), name="about"),
-    path('rooms/', views.RoomListView.as_view(), name='room-list'),
-    path('services/', views.ServicesListView.as_view(), name='services-list'),
-    path('experiences/', views.ExperiencesListView.as_view(), name='experiences-list'),
+    path('rooms/', views.RoomListView.as_view(), name='room_list'),
+    path('services/', views.ServicesListView.as_view(), name='services_list'),
+    path('experiences/', views.ExperiencesListView.as_view(), name='experiences_list'),
     path('room/<int:pk>/', views.RoomDetailView.as_view(), name='room-detail'),
-    path('service/<int:pk>/', views.ServiceDetailView.as_view(), name='service-detail'),
-    path('experience/<int:pk>/', views.ExperiencesDetailView.as_view(), name='experience-detail'),
+    path('service/<int:pk>/', views.ServiceDetailView.as_view(), name='service_detail'),
+    path('experience/<int:pk>/', views.ExperiencesDetailView.as_view(), name='experience_detail'),
     path('contact-form/', views.ContactFormView.as_view(), name='contact_form'),
     path('acommodation-types/', views.AcommodationTypes.as_view(), name='acommodation_types'),
 ]
