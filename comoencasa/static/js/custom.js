836e79819a49423436c8ff40888c241d6bf5d0e1
Daniel Montoya <dmontoya.web@gmail.com>
2019-03-24 19:07:50 -0500

    Updating changes, fixing issues

--- comoencasa/static/js/custom.js
+++ comoencasa/static/js/custom.js
@@ -38,36 +38,48 @@ $(document).ready(function() {
     });
 });
 
-//PORTFOLIO FILTER
-$(function() {
+    /*==================================================================
+    [ Isotope ]*/
+    var $topeContainer = $('.isotope-grid');
+    var $filter = $('.filter-tope-group');
 
-    var filterList = {
+    // filter items on button click
+    $filter.each(function () {
+        $filter.on('click', 'button', function () {
+            var filterValue = $(this).attr('data-filter');
+            $topeContainer.isotope({filter: filterValue});
+        });
 
-        init: function() {
+    });
 
-            // MixItUp plugin
-            // http://mixitup.io
-            $('#portfoliolist').mixItUp({
-                selectors: {
-                    target: '.portfolio',
-                    filter: '.filter'
-                },
-                load: {
-                    filter: '.logo'
+    // init Isotope
+    $(window).on('load', function () {
+        var $grid = $topeContainer.each(function () {
+            $(this).isotope({
+                itemSelector: '.isotope-item',
+                layoutMode: 'fitRows',
+                percentPosition: true,
+                animationEngine : 'best-available',
+                masonry: {
+                    columnWidth: '.isotope-item'
                 }
             });
+        });
+    });
 
-        }
+    var isotopeButton = $('.filter-tope-group button');
 
-    };
+    $(isotopeButton).each(function(){
+        $(this).on('click', function(){
+            for(var i=0; i<isotopeButton.length; i++) {
+                $(isotopeButton[i]).removeClass('how-active1');
+            }
 
-    // Run the show!
-    filterList.init();
+            $(this).addClass('how-active1');
+        });
+    });
 
-
-});
-
-
+    /*==================================================================*/
 
 //DATE PICKER	
 $(function() {
